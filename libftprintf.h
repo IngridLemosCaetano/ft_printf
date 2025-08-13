1acc7b2914c7fffcc6df591283e21e625ef826e5
IngridLemos <ingridlc42@gmail.com>
2025-08-13 16:30:53 -0300

    create function aux

---
+++ libftprintf.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilemos-c <ilemos-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/13 13:27:20 by ilemos-c          #+#    #+#             */
+/*   Updated: 2025/08/13 15:55:04 by ilemos-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+int		ft_type_converter(char *c);
+int		ft_count_converter(char *str);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+
+
+
+#endif
