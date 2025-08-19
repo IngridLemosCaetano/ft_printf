8bd4b2a9dd2aab13011a7cac62228ebb4e8a81ef
Ingrid Lemos Caetano Silva <115587425+IngridLemosCaetano@users.noreply.github.com>
2025-08-19 14:09:43 -0300

    Update ft_atoi.c

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -10,4 +10,47 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (nptr[i] == ' ' || nptr[i] == '\n' || nptr[i] == '\t'
+		|| nptr[i] == '\v' || nptr[i] == '\f' || nptr[i] == '\r')
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result = result * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+/*
+int main()
+{
+	char *ex1 = "  123";
+    char *ex2 = "-456";
+    char *ex3 = "+789";
+    char *ex4 = "42abc";
+    char *ex5 = "abc42";
+
+    printf("\"%s\" -> %d\n", ex1, ft_atoi(ex1));
+    printf("\"%s\" -> %d\n", ex2, ft_atoi(ex2));
+    printf("\"%s\" -> %d\n", ex3, ft_atoi(ex3));
+    printf("\"%s\" -> %d\n", ex4, ft_atoi(ex4));
+    printf("\"%s\" -> %d\n", ex5, ft_atoi(ex5));
+	return 0;
+}*/
 
