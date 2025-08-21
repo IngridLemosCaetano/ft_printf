c8f08d54398833172908980d09a4d86e0dd88ea8
IngridLemos <ingridlc42@gmail.com>
2025-08-21 12:31:57 -0300

    first version

--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: ilemos-c <ilemos-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 13:27:20 by ilemos-c          #+#    #+#             */
-/*   Updated: 2025/08/19 13:57:08 by ilemos-c         ###   ########.fr       */
+/*   Updated: 2025/08/19 15:29:31 by ilemos-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,11 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <stdarg.h>
+# include "libft/libft.h"
 
-int	ft_printf(const char *format, ...);
+int		ft_printf(const char *format, ...);
+size_t	ft_conversion(char c, va_list args);
+size_t	t_string(char *str);
 
 #endif
